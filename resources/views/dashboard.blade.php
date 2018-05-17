c17cc1895d7c3a1b710bd21dcdbf6d5bc48a528c
Ravgus <Alex1996saruman@yandex.ru>
2018-05-17 16:15:59 +0300

    1. Сделана Админка 2. Подкорректирована БД. 3. По мелочи
    Signed-off-by: Ravgus <Alex1996saruman@yandex.ru>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,12 @@
-<div style="background: url(images/bcgrd.png) no-repeat center center; min-height: 100vh;">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-4 col-xs-push-4 text-center">
+<div style="background: url(images/bcgrd.png) no-repeat center center;
+            min-height: 100vh;
+            border-top: 3px dotted #030101;
+	        border-bottom: 3px dotted #030101;
+	        box-shadow: inset 0 -1px 0 0 #030101, inset 0 1px 0 0 #030101, 0 1px 0 0 #030101, 0 -1px 0 0 #030101;
+	        margin-bottom: 1px;">
+    <div class="container-fluid">
+        <div class="row" style="text-align: center;">
+            <div class="col-xs-12">
                 <h1>Добро пожаловать!</h1>
             </div>
         </div>
